a901fab414366182b6a7313d3e7304e6ced46a78
nicoandreoli2000 <nicolasandreoli2000@hotmail.com>
2021-08-19 19:00:46 -0300

    My first commit

---
+++ EjerciciosComienzo.cpp
@@ -0,0 +1,74 @@
+#include "EjerciciosComienzo.h"
+
+int suma(int a, int b) {
+ 	// IMPLEMENTAR SOLUCION
+    return a + b;
+}
+
+void tablaDel(unsigned int tablaDel, unsigned int desde, unsigned int hasta) {
+ 	// IMPLEMENTAR SOLUCION
+}
+
+void simplificar(int n, int d) {
+	// IMPLEMENTAR SOLUCION
+}
+
+int ocurrencias123Repetidos(int* vector, int largo) {
+	// IMPLEMENTAR SOLUCION
+	return 0;
+}
+
+int maximoNumero(unsigned int n) {
+	// IMPLEMENTAR SOLUCION
+    return 0;
+}
+
+void ordenarVecInt(int *vec, int largoVec) {
+	// IMPLEMENTAR SOLUCION
+}
+
+
+char* invertirCase(char* str)
+{
+	// IMPLEMENTAR SOLUCION
+	return NULL;
+}
+
+int islas(char** mapa, int col, int fil){
+	// IMPLEMENTAR SOLUCION
+    return 0;
+}
+
+unsigned int ocurrenciasSubstring(char **vecStr, int largoVecStr, char *substr)
+{
+	// IMPLEMENTAR SOLUCION
+    return 0;
+}
+
+char **ordenarVecStrings(char **vecStr, int largoVecStr)
+{
+	// IMPLEMENTAR SOLUCION
+    return NULL;
+}
+
+int* intercalarVector(int* v1, int* v2, int l1, int l2){
+	// IMPLEMENTAR SOLUCION
+	return NULL;
+}
+
+bool subconjuntoVector(int* v1, int* v2, int l1, int l2)
+{
+	// IMPLEMENTAR SOLUCION
+	return false;
+}
+
+char** splitStr(char* str, char separador, int &largoRet)
+{
+	// IMPLEMENTAR SOLUCION
+	return NULL;
+}
+
+void ordenarVecIntMergeSort(int* vector, int largo) 
+{
+	// IMPLEMENTAR SOLUCION
+}
